3a60b962f744ab92c780fba9a247fe38c729c3ca
Steffen Moritz <steffen.moritz10@gmail.com>
2022-01-17 00:15:43 +0100

    First dummy function

---
+++ R/kssa.R
@@ -0,0 +1,19 @@
+#' @title Add two numbers
+#'
+#' @description Function to add two numbers
+#' @param x A number
+#' @param y A number
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+#'
+#' @export
+
+add <- function(x, y) {
+
+  z <- x + y
+  print("first function")
+
+  return(z)
+}
