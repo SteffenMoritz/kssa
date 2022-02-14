e766352f2ae9c3392407656b7e75358c651564cf
bdarocag <bdarocag@gmail.com>
2022-02-14 01:47:12 -0500

    # Starting implementation of Brayan's function and method selector system
    # It's a great progress, so i need think how solve in a future big requirement of any user. examp. many starting methods and many secondary methods
    
    #The most problematic is the return in that case

--- R/kssa.R
+++ R/kssa.R
@@ -12,15 +12,88 @@
 #' @import Metrics
 #' @import dplyr
 #' @import snpar
-#'
+#' @import zoo
 #' @export
 
-kssa <- function(x_ts, start.method = "interpolation", methods = c("auto.arima", "interpolation"), segments = 6, iterations = 30, metric) {
+kssa <- function(x_ts, #Time-series
+                 start.method, # Can select various
+                 methods, # Can select various
+                 segments = 6, #Number of segments to ts be divided
+                 iterations = 30, #Number of replicates
+                 percentmd = .3, #Percentage of new MD in simulations
+                 metric) { #Metrics to evaluate results
 
   # #' @import forecast
 
+  get_mdoriginal <- function(x, y){
+    na_original <- which(is.na(x))
+    weight_index <- rep(1, length(y))
+    weight_index[na_original] <- 0
+    return(weight_index)
+  }
 
-############### 1. IMPUT DATA FOR DIFFERENT SPECIES SEPEC #############
+  split_arbitrary <- function(x, percentmd, n_parts, mdoriginal){
+    size_window_B <- seq(from = round(length(x)/n_parts),
+                         to = length(x),
+                         by = round(length(x)/n_parts)+1)
+    size_window_A <- size_window_B - (n_parts + 1)
+    size_window_B <- c(size_window_B, length(x))
+    size_window_A <- c(size_window_A, size_window_B[length(size_window_B)-1]+1)
+    size_window_A[1] <- 1
+
+    index_time <- index(x)
+
+    chunks <- c()
+
+    for (i in 1:n_parts) {
+      chunk <- window(x = x, start = index_time[size_window_A[i]],
+                      end = index_time[size_window_B[i]])
+
+      m_a2 <- sample(x = chunk, size = (round(length(chunk)*percentmd)), replace = F,
+                     prob = mdoriginal[size_window_A[i]:size_window_B[i]])
+
+      chunk[index(m_a2)] <- NA
+
+      chunks <- append(chunks, chunk)
+    }
+    return(chunks)
+  }
+
+
+# 1. IMPUT DATA FOR DIFFERENT SPECIES SEPEC ####
+
+  df_of_methods <- data.frame("methods" =c("auto.arima", "StructTS", "linear",
+                                           "spline", "stine", "simple", "malinear",
+                                           "exponential", "kalman", "nalocf", "decomp"),
+                              "formulas" = c("na_kalman(x_ts,model='auto.arima',smooth = TRUE,nit = -1)",
+                                             "na_kalman(x_ts,model='StructTS',smooth = TRUE,nit = -1)",
+                                             "na_interpolation(x_ts,option='linear')",
+                                             "na_interpolation(x_ts,option='spline')",
+                                             "na_interpolation(x_ts,option='stine')",
+                                             "na_ma(x_ts,k=3,weighting = 'simple')",
+                                             "na_ma(x_ts,k=3,weighting = 'linear')",
+                                             "na_ma(x_ts,k=3,weighting = 'exponential')",
+                                             "na_seadec(x_ts,algorithm = 'kalman')",
+                                             "na_locf(x_ts,option = 'locf',na_remaining = 'rev')",
+                                             "na.interp(x_ts)")
+                              )
+
+  for (i in 1:length(df_of_methods$methods)) {
+    check <- start.method %in% df_of_methods$methods
+    if (all(check) == TRUE){
+      for (j in 1:length(start.method)) {
+        first_imputed <- eval(parse(text = df_of_methods$formulas[df_of_methods$methods == start.method[j]]))
+#Continue... [Here] <- El siguiente paso es introducir mi funcion, luego hay que ver el resultado contemplando una salida multiple de metodo inicial y de metodo secundario
+      }
+    }
+    else {
+      print(paste0("The methods '",
+                   paste(as.character(start.method[which(!check)]),
+                         collapse = ", "),
+                   "' in start.method parameter, are not in the list of available options"))
+
+    }
+  }
 
 # This is for the starting imputations
 if (start.method == "interpolation") {
@@ -48,8 +121,7 @@ else if (start.method == "auto.arima") {
 
 
 
-################################################################################
-#################### 2. VALIDATION WHEN SIMULATING THE NA'S STRUCTURE OF DATA IN CHUNKSSS ###############
+#2. VALIDATION WHEN SIMULATING THE NA'S STRUCTURE OF DATA IN CHUNKSSS ####
 
 
 #here we need a code line to split the time series and specify where to put simulation windows
