1470c17028920942ef382569bed9de3de3e979a4
bdarocag <bdarocag@gmail.com>
2022-05-23 15:13:11 -0500

    Correction on final imputation giving to the user.
    Add new comments between script lines
    
    New functionality to sort values by result on kssa_plot
    
    It's all

--- R/kssa.R
+++ R/kssa.R
@@ -16,15 +16,15 @@
 #' @import forecast
 #' @export
 
-kssa <- function(x_ts, #Time-series
-                 start.method, # Can select various
-                 methods, # Can select various
-                 segments, #Number of segments to ts be divided
-                 iterations, #Number of replicates
-                 percentmd = sample(x = 1:50, size = 1)/100,#Percentage of new MD in simulations
-                 seed = 1234) { #seed number
+kssa <- function(x_ts, #' Time-series
+                 start.method, #' Can select various
+                 methods, #' Can select various or all
+                 segments, #' Number of segments to ts be divided
+                 iterations, #' Replicate number
+                 percentmd = sample(x = 1:50, size = 1)/100, #' New Missing Data (MD) percentage in simulations
+                 seed = 1234) { #' Seed number
 
-  results <- data.frame( #Create data frame where put the final results
+  results <- data.frame( #' Create data frame where put the final results
     "start.method" = as.character(),
     "actual.method" = as.character(),
     "percent_md" = numeric(),
@@ -35,37 +35,37 @@ kssa <- function(x_ts, #Time-series
     "seed" = numeric()
     )
 
-#Function to get original positions of MD
+#' Function to get original positions of MD
   get_mdoriginal <- function(x, y){
-    na_original <- which(is.na(x)) #Allows get NA index
-    weight_index <- rep(1, length(y)) #Asign weigth of 1
-    weight_index[na_original] <- 0 #Asign weigth of 0 to original MD
-    return(weight_index) #Return index
+    na_original <- which(is.na(x)) #' Allows get NA index
+    weight_index <- rep(1, length(y)) #' Asign weigth of 1
+    weight_index[na_original] <- 0 #' Asign weigth of 0 to original MD
+    return(weight_index) #' Return index
   }
 
-#Function to split TS
-  split_arbitrary <- function(x, segments, mdoriginal){ #Arguments
-    size_window_B <- seq(#generate B point of time window
-      from = round(length(x)/segments), #from length / nparts
-      to = length(x), #to max length of TS
-      by = round(length(x)/segments)+1 #step by step
+#' Function to split TS
+  split_arbitrary <- function(x, segments, mdoriginal){ #' Arguments
+    size_window_B <- seq(#' generate B point of time window
+      from = round(length(x)/segments), #' from length / nparts
+      to = length(x), #' to max length of TS
+      by = round(length(x)/segments)+1 #' step by step
     )
-    size_window_A <- size_window_B + 1 #Generate A point of time window
-    size_window_B <- c(size_window_B, length(x)) #Put last one length
+    size_window_A <- size_window_B + 1 #' Generate A point of time window
+    size_window_B <- c(size_window_B, length(x)) #' Put last one length
     #size_window_A <- c(size_window_A, size_window_B[length(size_window_B)-1]+1)
-    size_window_A <- c(1, size_window_A) #Put firs point on side A
+    size_window_A <- c(1, size_window_A) #' Put firs point on side A
 
-    index_time <- index(x) #get indexes of window
+    index_time <- index(x) #' get indexes of window
 
-    chunks <- c() #Vector to contain segments
+    chunks <- c() #' Vector to contain segments
 
-    for (i in 1:segments) { #Run loop over segments
+    for (i in 1:segments) { #' Run loop over segments
       chunk <- window(x = x, start = index_time[size_window_A[i]], #create chunk
                       end = index_time[size_window_B[i]])
 
       #percentmd <- sample(x = 1:50, size = 1)/100
 
-      m_a2 <- sample(x = index(chunk), #Take new sample for simlate new MD
+      m_a2 <- sample(x = index(chunk), #' Take new sample for simlate new MD
                      size = (ceiling(length(chunk)*percentmd)), replace = F,
                      prob = mdoriginal[size_window_A[i]:size_window_B[i]])
 
@@ -78,7 +78,7 @@ kssa <- function(x_ts, #Time-series
     return(chunks)
   }
 
-  # Generate df of imputation methods and formulas
+  #' Generate df of imputation methods and formulas
   df_of_methods <- data.frame(
     "methods" =c("auto.arima", "StructTS", "linear",
                  "spline", "stine", "simple", "malinear",
@@ -107,7 +107,7 @@ kssa <- function(x_ts, #Time-series
                              "na.interp(newmdsimulation)")
   )
 
-  if(start.method == 'all'){
+  if(start.method == 'all'){ #' Define 'all' statement
     start.method <- c("auto.arima", "StructTS", "linear",
                       "spline", "stine", "simple", "malinear",
                       "exponential", "kalman", "nalocf", "decomp")
@@ -115,7 +115,7 @@ kssa <- function(x_ts, #Time-series
     start.method <- start.method
   }
 
-  if(methods == 'all'){
+  if(methods == 'all'){ #' Define 'all' statement
     methods <- c("auto.arima", "StructTS", "linear",
                       "spline", "stine", "simple", "malinear",
                       "exponential", "kalman", "nalocf", "decomp")
@@ -123,39 +123,39 @@ kssa <- function(x_ts, #Time-series
     methods <- methods
   }
 
-  #generate seeds
+  #' Generate seeds from starting seed
   set.seed(seed); seeds <- sample(1:9999, iterations)
 
-  #Check if start methods are in list of avaliable methods
+  #' Check if start methods are in list of avaliable methods
   check <- start.method %in% df_of_methods$methods
 
   if (all(check) == TRUE){
     for (i in 1:length(start.method)) {
-      # First imputation
+      #' First imputation
       set.seed(seed); first_imputed <- eval(parse(text = df_of_methods$formulas_x_ts[df_of_methods$methods == start.method[i]]))
 
-      # Get MD original
+      #' Get MD original
       mdoriginal <- get_mdoriginal(x = x_ts, y = first_imputed)
 
       for (k in 1:iterations) {
-        # Put new simulated MD
+        #' Put new simulated MD
         set.seed(seeds[k]); newmdsimulation <- split_arbitrary(x = first_imputed, segments = segments, mdoriginal = mdoriginal)
         na_count <- sum(is.na(newmdsimulation))/length(newmdsimulation)
         for (j in 1:length(methods)) {
-          #Check if selected methods are in list of avaliable methods
+          #' Check if selected methods are in list of avaliable methods
           check2 <- methods %in% df_of_methods$methods
 
           if (all(check) == TRUE){ # if all works correctly
-            # Actual imputation
+            #' Actual imputation
             set.seed(seeds[k]); actual_imputation <- eval(parse(text = df_of_methods$formulas_actual_ts[df_of_methods$methods == methods[j]]))#Here it goes good
 
-            # Get scores
+            #' Get scores
             rmse <- rmse(coredata(first_imputed),coredata(actual_imputation))
             cor <- cor(coredata(first_imputed),coredata(actual_imputation))^2
             mase <- mase(coredata(first_imputed),coredata(actual_imputation))
             smape <- smape(coredata(first_imputed),coredata(actual_imputation))
 
-            #Storage scores temporarly
+            #' Storage scores temporarly
             tempresults <- data.frame("start.method" = start.method[i],
                                       "actual.method" = methods[j],
                                       "percent_md" = na_count,
@@ -165,7 +165,7 @@ kssa <- function(x_ts, #Time-series
                                       "smape"= smape,
                                       "seed" = seeds[k])
 
-            # Append to final results
+            #' Append to final results
             results <- bind_rows(results, tempresults)
           }
           else {
@@ -185,6 +185,7 @@ kssa <- function(x_ts, #Time-series
                  "' in start.method parameter, are not in the list of available options"))
   }
 
+  #' Create summary of results
   summary_results <- results %>%
     group_by(start.method, actual.method) %>%
     summarise(mean_na = mean(percent_md),
@@ -199,13 +200,11 @@ kssa <- function(x_ts, #Time-series
               std_smape = sd(smape))
 
 
+  #' Get the best configuration & imputate original
   best_result <- results[which.min(results[,3]),]
-  set.seed(seed); first_imputed <- eval(parse(text = df_of_methods$formulas_x_ts[df_of_methods$methods == best_result$start.method]))
-  mdoriginal <- get_mdoriginal(x = x_ts, y = first_imputed)
-  set.seed(best_result$seed); newmdsimulation <- split_arbitrary(x = first_imputed, segments = segments, mdoriginal = mdoriginal)
-  set.seed(best_result$seed); actual_imputation <- eval(parse(text = df_of_methods$formulas_actual_ts[df_of_methods$methods == best_result$actual.method]))
-  rmse_final <- rmse(coredata(first_imputed),coredata(actual_imputation))
+  set.seed(seed); imputation <- eval(parse(text = df_of_methods$formulas_x_ts[df_of_methods$methods == best_result$actual.method]))
 
+  #' Configure results list
   results <- results[1:(length(results)-1)]
 
   results_df <- results
@@ -215,6 +214,6 @@ kssa <- function(x_ts, #Time-series
   class(summary_results) <- 'kssa.table'
 
 
-  list_results <- list(results, summary_results, results_df, summary_results_df, ts(actual_imputation), rmse_final)
+  list_results <- list(results, summary_results, results_df, summary_results_df, ts(imputation))
   return(list_results)
 }
--- R/kssa_plot.R
+++ R/kssa_plot.R
@@ -14,13 +14,14 @@
 #' @export
 
 kssa_plot <- function(
-  results, #results object from apply kssa function
-  type, #complete or summary
-  metric #if type == complete put any metric like rmse, cor, mase, smape
+  results, #' results object from apply kssa function
+  type, #' complete or summary
+  metric #' if type == complete put any metric like rmse, cor, mase, smape
 ){
-  #Fisrt condition with kssa.object
+  #' Fisrt condition with kssa.object
   if (class(results[[1]]) == 'kssa.table' & class(results[[2]]) == 'kssa.table'){
 
+    #' Extract complete data from kssa.tables object
     plot1 <- data.frame("start.method" = results[[1]]$start.method,
                               "actual.method" = results[[1]]$actual.method,
                               "rmse" = results[[1]]$rmse,
@@ -28,6 +29,7 @@ kssa_plot <- function(
                               "mase" = results[[1]]$mase,
                               "smape"= results[[1]]$smape)
 
+    #' Extract summary information from kssa.table object
     plot2 <- data.frame("start.method" = results[[2]]$start.method,
                         "actual.method" = results[[2]]$actual.method,
                         "mean_rmse" = results[[2]]$mean_rmse,
@@ -39,23 +41,29 @@ kssa_plot <- function(
                         "mean_smape"= results[[2]]$mean_smape,
                         "std_smape"= results[[2]]$std_smape)
 
+    #' Generates list of metrics
     list_of_metrics <- c("rmse", "cor", "mase", "smape")
+
+    #' Get check for validation process
     check <- metric %in% list_of_metrics
 
+
+    #' Process validation and if all clear, ---> Plot
     if (type == "complete"){
       if (all(check) == TRUE){
-        plot <- ggplot(data = plot1, aes(x = actual.method, y = eval(parse(text = metric)), fill = start.method)) +
+        plot <- ggplot(data = plot1, aes(x = reorder(actual.method, eval(parse(text = metric))), y = eval(parse(text = metric)), fill = start.method)) +
           geom_boxplot()+
           labs(title = "Complete plot", x = "Actual method", y = casefold(metric), fill = "Start method") +
           theme_classic()
       }
       else {
-        prin('metric must be "rmse","cor", "mase" or "smape"')
+        prin('metric must be "rmse","cor", "mase" or "smape".
+             Otherwise please check the defined metrics when run kssa')
       }
     }
     else if (type == "summary"){
       if (all(check) == TRUE){
-        plot <- ggplot(data = plot2, aes(x = actual.method, y = eval(parse(text = paste0('mean_',metric))), fill = start.method)) +
+        plot <- ggplot(data = plot2, aes(x = reorder(actual.method, eval(parse(text = paste0('mean_',metric)))), y = eval(parse(text = paste0('mean_',metric))), fill = start.method)) +
           geom_bar(stat="identity", color="black",
                    position=position_dodge()) +
           geom_errorbar(aes(ymin=eval(parse(text = paste0('mean_',metric)))-eval(parse(text = paste0('std_',metric))),
@@ -67,7 +75,8 @@ kssa_plot <- function(
 
       }
       else {
-        prin('metric must be "rmse","cor", "mase" or "smape"')
+        prin('metric must be "rmse","cor", "mase" or "smape".
+             Otherwise please check the defined metrics when run kssa')
         }
     }
     else {
@@ -78,4 +87,4 @@ kssa_plot <- function(
     print("Error: data must be a kssa.table object")
   }
   return(plot)
-}
+} #' End
